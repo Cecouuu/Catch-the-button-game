708acdc20168d29ecfb99cb25dd4eab390294f59
Cecouuu <cvetoslavkrumov2007@gmail.com>
2026-07-27 20:41:30 +0300

    Solve the problem for navigation bar to switch between main sections like a page switcher or also called "nav bar normal" but instead use method for sections switcher on 1 file. and to do! : fix java script file so the section switcher to work and nav bar to hack reaction!

--- scripts/navigationBar.js
+++ scripts/navigationBar.js
@@ -2,33 +2,42 @@ const homeBtn = document.querySelector("#homeBtn");
 const gameBtn = document.querySelector("#gameBtn");
 const aboutBtn = document.querySelector("#aboutBtn");
 const contactBtn = document.querySelector("#contactBtn");
+const homePageSection = document.querySelector("#homePageSection");
+const gamePageSection = document.querySelector("#gamePageSection");
+const aboutPageSection = document.querySelector("#aboutPageSection");
+const contactPageSection = document.querySelector("#contactPageSection");
 
 const navBarButtons = [
     {
         btn: homeBtn,
+        section: homePageSection,
     },
     {
         btn: gameBtn,
+        section: gamePageSection,
     },
     {
         btn: aboutBtn,
+        section: aboutPageSection,
     },
     {
         btn: contactBtn,
+        section: contactPageSection,
     },
 ]
 
 navBarButtons.forEach(function (button){
     button.addEventListener("click",function (){
-        btnSelection();
+        btnSelection(button.btn, button.section);
     });
 })
 
-function btnSelection(button){
+function btnSelection(buttonOnNavigationBar,sectionPage,){
     for(let i = 0; i < navBarButtons.length; i++){
-        button.style.backgroundColor = "transparent";
+        buttonOnNavigationBar.style.backgroundColor = "transparent";
     }
-    button.style.backgroundColor = "#00B7B5";
-    button.style.backgroundBlur = "20px";
-    button.style.border = "1px solid #00B7B5";
+    buttonOnNavigationBar.style.color = "red";
+    buttonOnNavigationBar.style.backgroundColor = "#00B7B5";
+    buttonOnNavigationBar.style.backgroundBlur = "20px";
+    buttonOnNavigationBar.style.border = "1px solid #00B7B5";
 }
