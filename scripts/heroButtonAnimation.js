903c7a9389b3633b1f3a5575f897b4dff13541fd
Cecouuu <cvetoslavkrumov2007@gamil.com>
2026-08-14 13:52:02 -0700

    Initial commit

---
+++ scripts/heroButtonAnimation.js
@@ -0,0 +1,37 @@
+const playBtn = document.querySelector("#playBtn");
+let blur = 0;
+let spread = 0;
+let boxShadowCount = 0;
+let growing = false;
+
+running = setInterval(() => {
+    playBtn.style.boxShadow = `0 0 ${blur}px ${spread}px rgb(0 255 241 / 0.25)`;
+        if (growing){
+            increaseShadow();
+        }
+        else{
+            decreaseShadow();
+        }
+}, 105);
+function increaseShadow (){
+    if (boxShadowCount >= 7) {
+        setTimeout(() => {
+            growing = false;
+        },125);
+        return;
+    }
+    boxShadowCount++;
+    blur+=2.25;
+    spread = blur / 1.5;
+}
+function decreaseShadow (){
+    if (boxShadowCount <= 0){
+        setTimeout (() => {
+            growing = true;
+        },450)
+        return;
+    }
+    boxShadowCount--;
+    blur-=2.25;
+    spread = blur / 1.5;
+}
---
+++ scripts/navigationBar.js
@@ -0,0 +1,46 @@
+const homeBtn = document.querySelector("#homeBtn");
+const gameBtn = document.querySelector("#gameBtn");
+const aboutBtn = document.querySelector("#aboutBtn");
+const contactBtn = document.querySelector("#contactBtn");
+const homePageSection = document.querySelector("#homePageSection");
+const gamePageSection = document.querySelector("#gamePageSection");
+const aboutPageSection = document.querySelector("#aboutPageSection");
+const contactPageSection = document.querySelector("#contactPageSection");
+
+const navBarButtons = [
+    {
+        btn: homeBtn,
+        section: homePageSection,
+    },
+    {
+        btn: gameBtn,
+        section: gamePageSection,
+    },
+    {
+        btn: aboutBtn,
+        section: aboutPageSection,
+    },
+    {
+        btn: contactBtn,
+        section: contactPageSection,
+    },
+]
+
+navBarButtons.forEach(function (button){
+    button.btn.addEventListener(`click`,() => {
+        btnSelection(button.btn, button.section);
+    });
+})
+
+function btnSelection(buttonOnNavBar,sectionPage,){
+    for(let i = 0; i < navBarButtons.length; i++){
+        // classList.add is adding a specific class from CSS to the HTML element.
+        // when is used classList it is selecting the classes that has or not the chosen element.
+        // if there is no element it will work  as well, and with add you can add classes to the element or remote it or toggle that adds and removes when is clicked!
+        navBarButtons[i].btn.classList.add("nav-list-button")
+        navBarButtons[i].btn.classList.remove("active");
+        navBarButtons[i].section.style.display = "none";
+    }
+    sectionPage.style.display = "flex";
+    buttonOnNavBar.classList.add("active");
+}
