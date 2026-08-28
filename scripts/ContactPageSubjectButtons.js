75f1e3dd5c7af502a95cef6d13ca29971f7cb7f9
Cecouuu <cvetoslavkrumov2007@gmail.com>
2026-08-28 16:51:45 +0300

    Making JS file for Buttons on contact page about subject changing text when buttons is clicked!

---
+++ scripts/ContactPageSubjectButtons.js
@@ -0,0 +1,20 @@
+const inputSubject = document.querySelector("#subject");
+
+const buttons = [
+    {
+        button: document.getElementsByClassName("bug-report"),
+    },
+    {
+        button: document.getElementsByClassName("suggestion"),
+    },
+    {
+        button: document.getElementsByClassName("collaborate"),
+    },
+    {
+        button: document.getElementsByClassName("general"),
+    },
+]
+
+buttons.forEach((button) => {
+
+})
